ee1520dc20a10f15955e6c00e098234cd6495ab0
noneXved <bajno.damian96@gmail.com>
2022-02-27 16:18:24 +0100

    Initial configuration

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,20 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { AboutComponent } from './about/about.component';
+import { CourseComponent } from './course/course.component';
+import { CourseDialogComponent } from './course-dialog/course-dialog.component';
+import { CousesCardListComponent } from './couses-card-list/couses-card-list.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AboutComponent,
+    CourseComponent,
+    CourseDialogComponent,
+    CousesCardListComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/couses-card-list/couses-card-list.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-couses-card-list',
+  templateUrl: './couses-card-list.component.html',
+  styleUrls: ['./couses-card-list.component.scss']
+})
+export class CousesCardListComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
