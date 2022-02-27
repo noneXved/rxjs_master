5845669ca9bd18aea3e15d56621648394b82060b
noneXved <bajno.damian96@gmail.com>
2022-02-27 19:11:53 +0100

    Created own observable & added config for mock data

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {fromEvent, noop, Observable, timer} from "rxjs";
 
 @Component({
   selector: 'app-about',
@@ -10,6 +11,44 @@ export class AboutComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+     const interval = timer(3000, 1000)
+
+    const stopWatch = interval.subscribe(val => console.log("stream 1:" + val))
+
+    setTimeout(() => {
+        stopWatch.unsubscribe()
+      }, 5000
+    );
+
+    const click = fromEvent(document, 'click');
+
+    click.subscribe(
+      evt => console.log(evt),
+
+      err => console.log(err),
+
+      () => console.log("completed")
+    );
+
+    // TODO HERE CREATED OWN OBSERVABLE
+    const http$ = Observable.create(observer => {
+        fetch('/api/courses').then(response => {
+          return response.json();
+        }).then(body => {
+          observer.next(body);
+
+          observer.complete();
+        }).catch(err => {
+          observer.error(err);
+        })
+    })
+
+    http$.subscribe(
+      courses => console.log(courses),
+      noop,
+      () => console.log('completed')
+
+    );
   }
 
 }
