aa8142e33450f89910d475d50ec4320db28ecdd8
noneXved <bajno.damian96@gmail.com>
2022-02-27 20:52:38 +0100

    Add courses component

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,15 +1,36 @@
 import { Component, OnInit } from '@angular/core';
+import {createHttpObservable} from "../common/util";
+import {map, noop} from "rxjs";
+import {Course} from "../model/course";
 
 @Component({
-  selector: 'app-home',
+  selector: 'home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
 
+  beginnerCourses: Course[] = [];
+
+  advancedCourses: Course[] = [];
+
   constructor() { }
 
   ngOnInit(): void {
+
+    const http$ = createHttpObservable('/api/courses');
+
+    const courses = http$.pipe(
+      map((res: any) => Object.values(res['payload'])
+      ));
+
+    // map
+    courses.subscribe(
+      courses => console.log(courses),
+      noop,
+      () => console.log('completed')
+    );
+
   }
 
 }
