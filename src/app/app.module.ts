ba5ab59126ea043675b5ead94924fe9b2fda0b75
noneXved <bajno.damian96@gmail.com>
2022-02-27 21:25:23 +0100

    Config routes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,9 +8,28 @@ import { CourseDialogComponent } from './course-dialog/course-dialog.component';
 import { HomeComponent } from './home/home.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { CoursesCardListComponent } from './courses-card-list/courses-card-list.component';
-import {RouterModule} from "@angular/router";
+import {Router, RouterModule} from "@angular/router";
 import {MatTabsModule} from "@angular/material/tabs";
 import {MatCardModule} from "@angular/material/card";
+import {MatDialog, MatDialogModule} from "@angular/material/dialog";
+import {Overlay, OverlayModule} from "@angular/cdk/overlay";
+import {AppRoutingModule} from "./app-routing.module";
+import {MatMenuModule} from "@angular/material/menu";
+import {MatIconModule} from "@angular/material/icon";
+import {MatSidenavModule} from "@angular/material/sidenav";
+import {MatButtonModule} from "@angular/material/button";
+import {MatSortModule} from "@angular/material/sort";
+import {MatProgressSpinnerModule} from "@angular/material/progress-spinner";
+import {MatDatepickerModule} from "@angular/material/datepicker";
+import {ReactiveFormsModule} from "@angular/forms";
+import {MatPaginatorModule} from "@angular/material/paginator";
+import {MatTableModule} from "@angular/material/table";
+import {MatSelectModule} from "@angular/material/select";
+import {MatMomentDateModule} from "@angular/material-moment-adapter";
+import {MatInputModule} from "@angular/material/input";
+import {MatToolbarModule} from "@angular/material/toolbar";
+import {MatListModule} from "@angular/material/list";
+import {HttpClientModule} from "@angular/common/http";
 
 @NgModule({
   declarations: [
@@ -25,8 +44,26 @@ import {MatCardModule} from "@angular/material/card";
     BrowserModule,
     BrowserAnimationsModule,
     RouterModule,
+    HttpClientModule,
+    MatMenuModule,
+    MatButtonModule,
+    MatIconModule,
+    MatCardModule,
     MatTabsModule,
-    MatCardModule
+    MatSidenavModule,
+    MatListModule,
+    MatToolbarModule,
+    MatInputModule,
+    MatTableModule,
+    MatPaginatorModule,
+    MatSortModule,
+    MatProgressSpinnerModule,
+    MatDialogModule,
+    AppRoutingModule,
+    MatSelectModule,
+    MatDatepickerModule,
+    MatMomentDateModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
