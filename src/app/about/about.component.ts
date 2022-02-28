16d2b85929be63c085f3582f1c0a4268d01b82be
noneXved <bajno.damian96@gmail.com>
2022-02-28 22:28:19 +0100

    concatMap() & concat() & of()

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -1,18 +1,20 @@
-import {Component, OnInit} from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { concat, of } from 'rxjs';
 
 @Component({
   selector: 'about',
   templateUrl: './about.component.html',
-  styleUrls: ['./about.component.scss']
+  styleUrls: ['./about.component.scss'],
 })
 export class AboutComponent implements OnInit {
-
-  constructor() {
-  }
+  constructor() {}
 
   ngOnInit(): void {
+    const source1 = of(1, 2, 3);
+    const source2 = of(4, 5, 6);
 
+    const result = concat(source1, source2);
+
+    result.subscribe(console.log);
   }
-
-
 }
--- src/app/course-dialog/course-dialog.component.ts
+++ src/app/course-dialog/course-dialog.component.ts
@@ -1,42 +1,69 @@
-import {AfterViewInit, Component, ElementRef, Inject, OnInit, ViewChild} from '@angular/core';
-import {FormBuilder, FormGroup, Validators} from "@angular/forms";
-import {Course} from "../model/course";
-import {MAT_DIALOG_DATA, MatDialogRef} from "@angular/material/dialog";
-import * as moment from "moment";
+import { AfterViewInit, Component, ElementRef, Inject, OnInit, ViewChild } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Course } from '../model/course';
+import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
+import * as moment from 'moment';
+import { concatMap, filter } from 'rxjs';
+import { fromPromise } from 'rxjs-compat/observable/fromPromise';
 
 @Component({
   selector: 'course-dialog',
   templateUrl: './course-dialog.component.html',
-  styleUrls: ['./course-dialog.component.scss']
+  styleUrls: ['./course-dialog.component.scss'],
 })
-export class CourseDialogComponent implements AfterViewInit {
-
+export class CourseDialogComponent implements AfterViewInit, OnInit {
   form: FormGroup;
 
-  course:Course;
+  course: Course;
 
   @ViewChild('saveButton', { static: true }) saveButton: ElementRef;
 
-  @ViewChild('searchInput', { static: true }) searchInput : ElementRef;
+  @ViewChild('searchInput', { static: true }) searchInput: ElementRef;
 
   constructor(
     private fb: FormBuilder,
     private dialogRef: MatDialogRef<CourseDialogComponent>,
-    @Inject(MAT_DIALOG_DATA) course:Course) {
-
+    @Inject(MAT_DIALOG_DATA) course: Course
+  ) {
     this.course = course;
 
     this.form = fb.group({
       description: [course.description, Validators.required],
       category: [course.category, Validators.required],
       releasedAt: [moment(), Validators.required],
-      longDescription: [course.longDescription,Validators.required]
+      longDescription: [course.longDescription, Validators.required],
     });
-
   }
 
-  ngAfterViewInit() {
+  ngOnInit() {
+    // TODO DEBUG FORM
+    this.form.valueChanges.subscribe(console.log);
 
+    // concatMap
+    // takes valueChanges
+    // -> creating new observables
+    // -> subscribing
+    // -> concatenating them
+    this.form.valueChanges
+      .pipe(
+        filter(() => this.form.valid),
+        concatMap((changes) => this.saveCourse(changes))
+      )
+      .subscribe();
+  }
+
+  ngAfterViewInit() {}
+
+  saveCourse(changes) {
+    return fromPromise(
+      fetch(`/api/courses/${this.course.id}`, {
+        method: 'PUT',
+        body: JSON.stringify(changes),
+        headers: {
+          'content-type': 'application/json',
+        },
+      })
+    );
   }
 
   // save() {
@@ -47,12 +74,9 @@ export class CourseDialogComponent implements AfterViewInit {
   //     );
   // }
 
-
-
-
   close() {
     this.dialogRef.close();
   }
 
-
+  save() {}
 }
