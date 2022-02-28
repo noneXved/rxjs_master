2a5e943b44624ae13566c607c58f46682fc255a0
noneXved <bajno.damian96@gmail.com>
2022-02-28 20:57:18 +0100

    Get values without Subscription & use tap() & shareReplay()

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,39 +1,32 @@
 import { Component, OnInit } from '@angular/core';
-import {createHttpObservable} from "../common/util";
-import {map, noop} from "rxjs";
-import {Course} from "../model/course";
+import { createHttpObservable } from '../common/util';
+import { map, Observable, shareReplay, tap } from 'rxjs';
+import { Course } from '../model/course';
 
 @Component({
   selector: 'home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  styleUrls: ['./home.component.scss'],
 })
 export class HomeComponent implements OnInit {
-
-  beginnerCourses: Course[] = [];
-
-  advancedCourses: Course[] = [];
-
-  constructor() { }
+  beginnerCourses: Observable<Course[]>;
+  advancedCourses: Observable<Course[]>;
 
   ngOnInit(): void {
-
-    const http$ = createHttpObservable('/api/courses');
+    const http$: Observable<Course[]> = createHttpObservable('/api/courses');
 
     const courses = http$.pipe(
-      map((res: any) => Object.values(res['payload'])
-      ));
-
-    // map
-    courses.subscribe(
-      courses => {
-        this.beginnerCourses = courses.filter(course => course.category == "BEGINNER")
-        this.advancedCourses = courses.filter(course => course.category == "ADVANCED")
-      },
-      noop,
-      () => console.log('completed')
+      tap(() => console.log('The request was executed')),
+      map((res: any) => Object.values(res['payload'])),
+      shareReplay()
     );
 
+    courses.subscribe();
+    this.beginnerCourses = courses.pipe(
+      map((courses: any) => courses.filter((course) => course.category == 'BEGINNER'))
+    );
+    this.advancedCourses = courses.pipe(
+      map((courses: any) => courses.filter((course) => course.category == 'ADVANCED'))
+    );
   }
-
 }
