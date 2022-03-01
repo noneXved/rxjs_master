baeb72b16865ea2e1f42c89accdb1633e76f8ace
noneXved <bajno.damian96@gmail.com>
2022-03-01 22:26:22 +0100

    Catching errors **RETRY**

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,6 +1,18 @@
 import { Component, OnInit } from '@angular/core';
 import { createHttpObservable } from '../common/util';
-import {catchError, finalize, map, Observable, of, shareReplay, tap, throwError} from 'rxjs';
+import {
+  catchError,
+  delay,
+  delayWhen,
+  finalize,
+  map,
+  Observable,
+  of,
+  retryWhen,
+  shareReplay,
+  tap,
+  throwError, timer
+} from 'rxjs';
 import { Course } from '../model/course';
 
 @Component({
@@ -16,20 +28,14 @@ export class HomeComponent implements OnInit {
     const http$: Observable<Course[]> = createHttpObservable('/api/courses');
 
     const courses = http$.pipe(
-      // catching error at the begging prevent to realize rest request
-      // shareReplay() shares result between many calls - optimization
-      catchError(error => {
-          console.log("Error occured", error)
-
-          return throwError(error);
-        }
-      ),
-      finalize(() =>
-        console.log("aaa")
-      ),
       tap(() => console.log('The request was executed')),
       map((res: any) => Object.values(res['payload'])),
-      shareReplay()
+      shareReplay(),
+      retryWhen(errors => errors.pipe(
+        delayWhen(() => timer(2000))
+        // delay(2000)
+        )
+      )
     );
 
     courses.subscribe();
