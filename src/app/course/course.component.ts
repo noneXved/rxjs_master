f0bdfd2c2f3cff7b0ab864c7638b3206f9bd84de
noneXved <bajno.damian96@gmail.com>
2022-03-01 21:45:48 +0100

    switchMap() & trick to automatically reload in search

--- src/app/course/course.component.ts
+++ src/app/course/course.component.ts
@@ -1,6 +1,6 @@
 import { AfterViewInit, Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
-import {debounceTime, distinctUntilChanged, fromEvent, map, Observable} from 'rxjs';
+import {concat, debounceTime, distinctUntilChanged, fromEvent, map, Observable, switchMap} from 'rxjs';
 import { Course } from '../model/course';
 import { Lesson } from '../model/lesson';
 import { createHttpObservable } from '../common/util';
@@ -11,6 +11,7 @@ import { createHttpObservable } from '../common/util';
   styleUrls: ['./course.component.scss'],
 })
 export class CourseComponent implements OnInit, AfterViewInit {
+  courseId: string;
   course: Observable<Course>;
   lessons: Observable<Lesson[]>;
 
@@ -19,23 +20,36 @@ export class CourseComponent implements OnInit, AfterViewInit {
   constructor(private route: ActivatedRoute) {}
 
   ngOnInit() {
-    const courseId = this.route.snapshot.params['id'];
+    this.courseId = this.route.snapshot.params['id'];
 
     // Usage route path into Observable
     this.course = createHttpObservable(`/api/courses/1`);
 
-    this.lessons = createHttpObservable(`/api/lessons?courseId=1&pageSize=100`).pipe(map((res: any) => res['payload']));
   }
 
   ngAfterViewInit(): void {
-    fromEvent(this.input.nativeElement, 'keyup')
+     const searchLessons = fromEvent(this.input.nativeElement, 'keyup')
       .pipe(
         map((event: any) => event.target.value),
         // it waits a period of time in example 0,4sec to send data
         debounceTime(400),
         // it prevent duplicates
-        distinctUntilChanged()
+        distinctUntilChanged(),
+        // TODO switchMap allows to cancel previous request to optimize
+        // if we provide some data into input it will cancel previous requests to improve app
+        switchMap((search:any) => this.loadLessons(search))
+      )
 
-      ).subscribe(console.log);
+    const initialLessons = this.loadLessons();
+
+    this.lessons = concat(initialLessons, searchLessons);
+  }
+
+  loadLessons(search = ''): Observable<Lesson[]> {
+    return createHttpObservable(
+      `/api/lessons?courseId=1&pageSize=100&filter=${search}`)
+      .pipe(
+        map((res: any) => res['payload'])
+      );
   }
 }
