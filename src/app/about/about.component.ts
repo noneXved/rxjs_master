f820b3b218cba80c3f5987090780dd8b5ba8f980
noneXved <bajno.damian96@gmail.com>
2022-03-04 21:16:46 +0100

    BehaviorSubject & AsyncSubject & ReplaySubject

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import {concat, interval, map, merge, of, Subject} from 'rxjs';
-import {createHttpObservable} from "../common/util";
+import { BehaviorSubject } from 'rxjs';
 
 @Component({
   selector: 'about',
@@ -11,13 +10,25 @@ export class AboutComponent implements OnInit {
   constructor() {}
 
   ngOnInit(): void {
-    const subject = new Subject()
+    // Behavior subject must always take parameter and
+    const subject = new BehaviorSubject(0);
+    // BehaviorSubject - is running naturally like we put 0 so our early sub get: 0 1 2 3 and late sub get: 3 - last value
+    // AsyncSubject - always waiting for all observables to retrieve LAST VALUE fore early: 3 and last sub: 3
+    // ReplaySubject - repeat all observables for subscribed values for early sub: 1 2 3 & late sub: 1 2 3
     const observable = subject.asObservable();
-    observable.subscribe(console.log);
+    observable.subscribe((val) => console.log('early sub: ' + val));
 
-    subject.next(1)
-    subject.next(2)
-    subject.next(3)
-    subject.complete()
+    subject.next(1);
+    subject.next(2);
+    subject.next(3);
+
+    // it prevents to emit value in late sub cause it ends the SUBJECT
+    subject.complete();
+
+    setTimeout(() => {
+      // without subject.complete() it returns last value because we use BehaviorSubject()
+      // in common Subject we don't get value here
+      observable.subscribe((val) => console.log('late sub: ' + val));
+    });
   }
 }
