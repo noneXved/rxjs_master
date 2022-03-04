419b2e713abee1995518a89dd01503015cd34a79
noneXved <bajno.damian96@gmail.com>
2022-03-04 20:58:29 +0100

    [Subject] Best way to create own observable

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import {concat, interval, map, merge, of} from 'rxjs';
+import {concat, interval, map, merge, of, Subject} from 'rxjs';
 import {createHttpObservable} from "../common/util";
 
 @Component({
@@ -11,33 +11,13 @@ export class AboutComponent implements OnInit {
   constructor() {}
 
   ngOnInit(): void {
+    const subject = new Subject()
+    const observable = subject.asObservable();
+    observable.subscribe(console.log);
 
-    // concat
-    const source1 = of(1, 2, 3);
-    const source2 = of(4, 5, 6);
-
-    const resultConcat = concat(source1, source2);
-
-    resultConcat.subscribe(console.log);
-
-    // merge
-    const interval1 = interval(1000);
-    const interval2 = interval1.pipe(map(value => value * 10))
-
-    const resultMerge = merge(interval1, interval2);
-    console.log(resultMerge)
-
-    // unsubscribe - if stream is never ending, unsubscribe automatically ends it
-    const intervalUnsub = interval(1000);
-    const sub = intervalUnsub.subscribe(console.log)
-
-    setTimeout(() => sub.unsubscribe(), 5000)
-
-
-    const http = createHttpObservable('/api/courses');
-
-    const subscribe = http.subscribe(console.log)
-
-    setTimeout(() => subscribe.unsubscribe(), 0)
+    subject.next(1)
+    subject.next(2)
+    subject.next(3)
+    subject.complete()
   }
 }
